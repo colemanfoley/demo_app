bde8fee567ce9509c79623d61c9faf3ac990967c
Coleman Foley <colemanfoley@gmail.com>
2013-09-08 12:55:36 -0700

    Made Micropost scaffold.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130908164226) do
+ActiveRecord::Schema.define(version: 20130908195458) do
+
+  create_table "microposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
